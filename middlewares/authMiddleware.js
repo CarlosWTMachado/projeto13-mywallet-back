cdac2bccceb048a01738dae0da2145d75f79fcc6
Carlos William <carloswtmachado@gmail.com>
2022-07-03 22:08:45 -0300

    confirma senha

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,10 +4,11 @@ export async function validaCadastro(req, res, next) {
 	const cadastroSchema = joi.object({
 		nome: joi.string().required(),
 		email: joi.string().email().required(),
-		senha: joi.string().required()
+		senha: joi.string().required(),
+		confirma_senha: joi.string().required()
 	});
 	const validation = cadastroSchema.validate(req.body, {abortEarly: false});
-	if(validation.error) return res.status(422).send(validation.error.details.map(v => v.message));
+	if(validation.error || senha != confirma_senha) return res.status(422).send(validation?.error.details.map(v => v.message));
 	next();
 }
 
